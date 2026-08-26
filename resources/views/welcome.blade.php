62e6d901a463b7c97ac755070eef17c1c1a4ac3e
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-08-26 21:46:04 +0530

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    @fonts
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        WELCOME PAGE
+    </h1>
+</body>
+
+</html>
