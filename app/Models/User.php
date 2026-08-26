636ad3cba0d2bb67e4ea41454ea5428cc664937b
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-08-26 22:20:31 +0530

    feat(auth): install DevDojo Auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Devdojo\Auth\Models\User as AuthUser;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends AuthUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
