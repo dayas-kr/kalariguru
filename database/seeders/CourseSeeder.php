4c91d35902d8dcf866db69f6652bce4bd3da1d96
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-09-01 23:17:47 +0530

    feat(courses): expand CourseSeeder to include duration and semester count

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -10,14 +10,38 @@ class CourseSeeder extends Seeder
     public function run(): void
     {
         $courses = [
-            ['name' => 'Kalaripayattu Basic Course', 'slug' => 'kalaripayattu-basic-course'],
-            ['name' => '3 Year Kalaripayattu Certificate Course', 'slug' => '3-year-kalaripayattu-certificate-course'],
-            ['name' => '6 Year Kalaripayattu Certificate Course', 'slug' => '6-year-kalaripayattu-certificate-course'],
-            ['name' => '12 Year Kalaripayattu Certificate Course', 'slug' => '12-year-kalaripayattu-certificate-course'],
+            [
+                'name' => 'Kalaripayattu Basic Course',
+                'slug' => 'kalaripayattu-basic-course',
+                'duration' => '21 days',
+                'semester_count' => 1
+            ],
+            [
+                'name' => '3 Year Kalaripayattu Certificate Course',
+                'slug' => '3-year-kalaripayattu-certificate-course',
+                'duration' => '3 years',
+                'semester_count' => 12
+            ],
+            [
+                'name' => '6 Year Kalaripayattu Certificate Course',
+                'slug' => '6-year-kalaripayattu-certificate-course',
+                'duration' => '6 years',
+                'semester_count' => 24
+            ],
+            [
+                'name' => '12 Year Kalaripayattu Certificate Course',
+                'slug' => '12-year-kalaripayattu-certificate-course',
+                'duration' => '12 years',
+                'semester_count' => 48
+            ],
         ];
 
         foreach ($courses as $course) {
-            Course::create(['name' => $course['name'], 'slug' => $course['slug']]);
+            Course::create([
+                'name' => $course['name'],
+                'slug' => $course['slug'],
+                'duration' => $course['duration'],
+            ]);
         }
     }
 }
